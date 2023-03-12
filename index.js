803b4d761495abdfe821c3b2b5a624d414155992
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-12 17:26:26 -0400

    api.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api', (req, res) => res.send('Its working!'));
+
 
 app.listen(process.env.port || 3000, function(){
     console.log('now listening for requests');
