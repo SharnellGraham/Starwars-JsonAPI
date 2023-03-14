a1a26af864fb5a6e66cbb35a293497e44cae7a54
Sharnell Graham <goose@Sharnells-MBP.attlocal.net>
2023-03-13 20:06:24 -0400

    Plan dir included with index.js coding

---
+++ planning/lib/index.js
@@ -0,0 +1,20 @@
+// imports
+import express from "express";
+const app = express()
+
+app.use(express.json()) // This line allows your server to accept incoming JSON data in the body of the request.
+                        // Without this line, you won't be able to parse JSON data sent to the server.
+
+// routes
+app.get('/', async (req, res) => { // This line sets up a GET request that listens for requests going to your server's root URL ('/')
+    res.send('hello humans') 
+                         
+
+})
+
+// starting the server
+app.listen(3000, () => { // This line starts your Express server and specifies that it should listen on port 3000.
+                         
+console.log('May the force be with you xD ') 
+// This message is logged to the console once the server has started listening for incoming requests.
+})
